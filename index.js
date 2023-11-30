bf8bd9150c3d1e71fd57ced192bb14ed65e797e6
davidmelendez <Dmelz11@github.com>
2023-11-29 21:55:33 -0800

    initial commit

---
+++ index.js
@@ -0,0 +1,52 @@
+const fs = require('fs');
+
+const inquirer = require('inquirer');
+
+import inquirer from 'inquirer';
+
+const generateMarkdown = require('./utils/generateMarkdown');
+
+
+inquirer
+.prompt([
+  {
+    type: "input",
+    message: "What is the name and description of this project?",
+    name: "Title"
+  },
+  {
+    type: "input",
+    message: "Why did you create this project?",
+    name: "why"
+  },
+  {
+    type: "input",
+    message:"What problem does it solve?",
+    name: "problem"
+  },
+  {
+    type: "input",
+    message: "What did you learn?",
+    name: "learning"
+  },
+  {
+    type: "input",
+    message: "How does your project stand out?",
+    name: "features"
+},
+{
+    type: "input",
+    message: "What were some challenges?",
+    name: "challenges"
+},
+]).then((data) =>{
+console.log(data);
+fs.writeFile('README.md',generateMarkdown(data),
+error => {
+    if (error){
+        console.log('Please finish inputting all data.')
+    }
+    console.log('Your README has been generated')
+}
+)})
+
